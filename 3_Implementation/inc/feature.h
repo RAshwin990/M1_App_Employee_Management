75924b3c2baf33e943f409261d37257b03e0665f
RAshwin990 <81633037+RAshwin990@users.noreply.github.com>
2021-11-25 09:18:23 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,2 +1,16 @@
+#ifndef FEATURE_H
+#define FEATURE_H
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h> 
+#include <string.h> 
+
 void gotoxy()
 int main()
+struct emp
+char empname[]
+long int recsize
+fp = fopen()
+recsize = sizeof()
